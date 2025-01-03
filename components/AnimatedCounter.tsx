afd6b6073ef6fe47ce9211428ea6cad9024bcb9e
Sakan <sakan.rodjanai@gmail.com>
2025-01-03 19:54:47 +0700

    Implement the complete layout

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,12 @@
+"use client";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp prefix="$" decimals={2} decimal="," end={amount} />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
